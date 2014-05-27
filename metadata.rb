b34938d71132404506e39884a53a91797b74bb9b
Okura Masafumi <masafumi.o1988@gmail.com>
2014-05-27 20:55:15 +0900

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'achiiibot'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures achiiibot'
+long_description 'Installs/Configures achiiibot'
+version          '0.1.0'
+
