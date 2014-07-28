f2ac2aa1a9658966d54ab097dade5da655b266e3
Okura Masafumi <masafumi.o1988@gmail.com>
2014-07-28 16:49:39 +0900

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'masafumi@achiiive.com'
 license          'All rights reserved'
 description      'Installs/Configures achiiibot'
 long_description 'Installs/Configures achiiibot'
-version          '0.2.7'
+version          '0.2.8'
 
 depends 'git'
 depends 'nodejs'
